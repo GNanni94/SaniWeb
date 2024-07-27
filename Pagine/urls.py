a5e4e741e1bb56c57acdbf474b6cf8577aefa4bd
francesco0123 <frank-1996@hotmail.it>
2024-07-27 16:40:03 +0200

    pdf etichettaturaAmbientale

--- Pagine/urls.py
+++ Pagine/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name = "home"),
     path("home/", TemplateView.as_view(template_name="home.html"), name = "home"),
     path("Storia Azienda", TemplateView.as_view(template_name="storia_azienda.html"), name = "storia_azienda"),
-    path("etichettatura ambientale",TemplateView.as_view(template_name="etichettatura_ambientale.html"), name = "etichettatura_ambientale"),
+    path("etichettaturaAmbientale",TemplateView.as_view(template_name="etichettatura_ambientale.html"), name = "etichettatura_ambientale"),
     #path("Contatti", TemplateView.as_view(template_name="contatti.html"), name = "contatti"),
     path("Contatti", include("Utente.urls")),
     path("Documenti", DocumentoView.as_view(), name="documenti"),
