887ea26300e047b627b11ac3aeabba322e702ec4
francesco0123 <frank-1996@hotmail.it>
2024-07-27 17:27:51 +0200

    logout

--- Utente/urls.py
+++ Utente/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(
             template_name="registration/login.html",
             authentication_form=CustomAuthenticationForm
-            ),name='login')
+            ),name='login'),
+    path('logout/', views.LogoutView.as_view(),name='logout')
 ]
