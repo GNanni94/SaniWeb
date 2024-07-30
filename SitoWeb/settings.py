620d9e4d9c05a73e97231006ebb4980e0e8ff410
francesco0123 <frank-1996@hotmail.it>
2024-07-30 12:12:50 +0200

    rimossa password

--- SitoWeb/settings.py
+++ SitoWeb/settings.py
@@ -158,6 +158,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'giulionannicini@gmail.com'
-EMAIL_HOST_PASSWORD = 'ikbdqnbxdtezozwd'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
