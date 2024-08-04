dda7ed70b73a51531835c55b32704456f5191e00
GNanni94 <127394457+GiulioNannicini@users.noreply.github.com>
2024-08-04 22:48:39 +0200

    email sani

--- SitoWeb/settings.py
+++ SitoWeb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--q!k-c1bfeebws@4rc#%_t!ae_t^fn-)!gqzo#t&grj&fb1a@b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.107']
 
 
 # Application definition
