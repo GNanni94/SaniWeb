8ed14d345f5b00de12cd9b64d82096ab8a166c37
francesco0123 <frank-1996@hotmail.it>
2024-08-05 16:59:18 +0200

    override del metodo form_valid, usato che fare qualcosa dopo la validaazione dei campi è corretta (in questo caso, lo scopo è l'invio della email)

--- Utente/views.py
+++ Utente/views.py
@@ -1,6 +1,7 @@
+from django.forms import BaseModelForm
 from django.shortcuts import render, redirect, HttpResponse
 from .forms import ClienteForm, MessaggioForm
-from .models import Cliente, Messaggi, Richiesta_messaggio
+from .models import Cliente, Messaggi, Richiesta_messaggio, Registrati
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import ClienteCreationForm
@@ -34,3 +35,18 @@ class SignUpView(CreateView):
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
+    def form_valid(self, form) -> HttpResponse:
+        print(form.cleaned_data)
+        form_cleaned = form.cleaned_data
+        registrato = Registrati()
+        registrato.email = form_cleaned.get("email")
+        registrato.first_name = form_cleaned.get("first_name")
+        registrato.cognome_ragione_sociale = form_cleaned.get("cognome_ragione_sociale")
+        registrato.codiceFiscale_PartitaIVA = form_cleaned.get("codiceFiscale_PartitaIVA")
+        registrato.indirizzo = form_cleaned.get("indirizzo")
+        registrato.citta = form_cleaned.get("citta")
+        registrato.telefono = form_cleaned.get("telefono")
+        registrato.password = form_cleaned.get("password1")
+        
+        registrato.email_user()
+        return super().form_valid(form)
